c2d3c4351fcbacca9101edd89a7b2ff841bfe966
Abel <gamergeek1107@gmail.com>
2025-11-23 13:38:43 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Nebula-Chat/',   /https://shinobiscripter.github.io/Nebula-Chat
-/
+  base: '/Nebula-Chat/',
 });
